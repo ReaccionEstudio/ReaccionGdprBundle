bfd733f4932f141e43d5ac017565899fd2903c58
viher3 <viher_3@hotmail.com>
2019-01-12 19:21:12 +0100

    Created src folder

---
+++ src/ReaccionGdprBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the ReaccionGdprBundle package.
+ *
+ * (c) Reaccion Estudio <https://www.reaccionestudio.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ReaccionEstudio\GdprBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ReaccionGdprBundle class.
+ *
+ * @author Alberto Vian <alberto@reaccionestudio.com>
+ */
+class ReaccionGdprBundle extends Bundle
+{
+}
